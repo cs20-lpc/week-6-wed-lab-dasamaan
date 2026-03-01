9b93acc08e9a3ad917db695b763b94e54f702a9d
dasamaan <dasamaan@zonemail.clpccd.edu>
2026-02-28 22:50:22 -0800

    done

--- lab08.cpp
+++ lab08.cpp
@@ -6,11 +6,20 @@ using namespace std;
  * Function prototypes
 *******************************************************************************/
 
-unsigned fact(unsigned);
-unsigned fib(unsigned);
-unsigned mult(unsigned, unsigned);
-unsigned power(unsigned, unsigned);
-unsigned product(unsigned, unsigned);
+unsigned fact(unsigned n);
+unsigned factTail(unsigned n, unsigned acc);
+
+unsigned fib(unsigned n);
+unsigned fibTail(unsigned n, unsigned a, unsigned b);
+
+unsigned mult(unsigned x, unsigned y);
+unsigned multTail(unsigned x, unsigned y, unsigned acc);
+
+unsigned power(unsigned x, unsigned y);
+unsigned powerTail(unsigned x, unsigned y, unsigned acc);
+
+unsigned product(unsigned x, unsigned y);
+unsigned productTail(unsigned x, unsigned y, unsigned acc);
 
 /*******************************************************************************
  * Description:
@@ -46,61 +55,54 @@ int main() {
  * TODO: make them tail recursive :)
 *******************************************************************************/
 
+// Factorial: n! = n*(n-1)!
 unsigned fact(unsigned n) {
-    // base cases (combined)
-    if (n <= 1) {
-        return 1;
-    }
-
-    // recursive case
-    unsigned res = fact(n - 1);
-    return res * n;
+    return factTail(n, 1);
 }
 
+unsigned factTail(unsigned n, unsigned acc) {
+    if (n <= 1) return acc;
+    return factTail(n - 1, acc * n);
+}
+
+// Fibonacci: fib(n) = fib(n-1) + fib(n-2)
 unsigned fib(unsigned n) {
-    // base case 1
-    if (n == 0) {
-        return 0;
-    }
-
-    // base case 2
-    else if (n == 1) {
-        return 1;
-    }
-
-    // recursive case
-    return fib(n - 1) + fib(n - 2);
+    return fibTail(n, 0, 1);
 }
 
+// a = fib(n-2), b = fib(n-1)
+unsigned fibTail(unsigned n, unsigned a, unsigned b) {
+    if (n == 0) return a;
+    if (n == 1) return b;
+    return fibTail(n - 1, b, a + b);
+}
+
+// Multiplication using repeated addition
 unsigned mult(unsigned x, unsigned y) {
-    // base case
-    if (y == 0) {
-        return 0;
-    }
-
-    // recursive case
-    unsigned res = mult(x, y - 1);
-    return res + x;
+    return multTail(x, y, 0);
 }
 
+unsigned multTail(unsigned x, unsigned y, unsigned acc) {
+    if (y == 0) return acc;
+    return multTail(x, y - 1, acc + x);
+}
+
+// Power: x^y
 unsigned power(unsigned x, unsigned y) {
-    // base case
-    if (y == 0) {
-        return 1;
-    }
-
-    // recursive case
-    unsigned res = power(x, y - 1);
-    return res * x;
+    return powerTail(x, y, 1);
 }
 
-unsigned product(unsigned x, unsigned y) {
-    // base case
-    if (x == y) {
-        return x;
-    }
+unsigned powerTail(unsigned x, unsigned y, unsigned acc) {
+    if (y == 0) return acc;
+    return powerTail(x, y - 1, acc * x);
+}
 
-    // recursive case
-    unsigned p = product(x + 1, y);
-    return p * x;
+// Product of consecutive numbers from x to y
+unsigned product(unsigned x, unsigned y) {
+    return productTail(x, y, 1);
+}
+
+unsigned productTail(unsigned x, unsigned y, unsigned acc) {
+    if (x > y) return acc;  // base case
+    return productTail(x + 1, y, acc * x);
 }
